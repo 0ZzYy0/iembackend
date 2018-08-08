971810187f2618681735c1e194950a112f359f46
0ZzYy0 <553224182@qq.com>
2018-08-08 15:51:46 +0800

    地质灾害调查导出

--- src/main/java/com/iem/safety/iembackend/geologydisaster/GeologyDisasterController.java
+++ src/main/java/com/iem/safety/iembackend/geologydisaster/GeologyDisasterController.java
@@ -1,7 +1,10 @@
 package com.iem.safety.iembackend.geologydisaster;
 
+import com.iem.safety.iembackend.intensitysurvey.Intensitysurvey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -28,4 +31,34 @@ public class GeologyDisasterController {
         return repository.saveAll(lists);
     }
 
+    @RequestMapping(value = "getbyoidandeid", method = RequestMethod.GET)
+    public List<GeologyDisaster> getByOidAndTSLg(@RequestParam("ownerId") String ownerId,
+                                                 @RequestParam("earthquakeId") String earthquakeId) throws IOException {
+        return repository.findByOwnerIdAndEarthquakeId(ownerId, earthquakeId);
+    }
+
+    @RequestMapping(value = "delbyid", method = RequestMethod.POST)
+    public void delById(@RequestParam("id") String id) {
+        repository.deleteGeologyDisasterById(id);
+    }
+
+    @RequestMapping(value = "admin/exportGeologyDisasterAll", method = RequestMethod.GET)
+    public void exportGeologyDisasterAll(@RequestParam("eid") String eid,
+                                     HttpServletResponse res) {
+        service.exportGeologyDisasterAll(eid, res);
+    }
+
+
+    @RequestMapping(value = "admin/exportGeologyDisasterByGroup", method = RequestMethod.GET)
+    public void exportGeologyDisasterByGroup(@RequestParam("eid") String eid,
+                                             HttpServletResponse res) {
+        service.exportGeologyDisasterByGroup(res, eid);
+    }
+
+    @RequestMapping(value = "customer/exportGeologyDisasterByEid", method = RequestMethod.GET)
+    public void exportCustomerGeologyDisasterByEid(@RequestParam("eid") String eid,
+                                               @RequestParam("cid") String cid,
+                                               HttpServletResponse res) {
+        service.exportCustomerGeologyDisasterByEid(eid, cid, res);
+    }
 }
--- src/main/java/com/iem/safety/iembackend/geologydisaster/GeologyDisasterRepository.java
+++ src/main/java/com/iem/safety/iembackend/geologydisaster/GeologyDisasterRepository.java
@@ -13,10 +13,10 @@ public interface GeologyDisasterRepository extends MongoRepository<GeologyDisast
 
     //List<GeologyDisaster> findByOwnerIdAndTimestampGreaterThan(String ownerId, long timestamp);
 
-   //List<GeologyDisaster> findByOwnerIdAndEarthquakeId(String ownerId, String earthquakeId);
+    List<GeologyDisaster> findByOwnerIdAndEarthquakeId(String ownerId, String earthquakeId);
 
-    //List<GeologyDisaster> findGeologyDisasterByEarthquakeIdAndOwnerIdAndStatus(String earthquakeId, String customerId, String status);
+    List<GeologyDisaster> findGeologyDisasterByEarthquakeIdAndOwnerIdAndStatus(String earthquakeId, String customerId, String status);
 
-    //List<GeologyDisaster> findGeologyDisasterByEarthquakeIdAndStatus(String earthquakeId, String status);
+    List<GeologyDisaster> findGeologyDisasterByEarthquakeIdAndStatus(String earthquakeId, String status);
 
 }
